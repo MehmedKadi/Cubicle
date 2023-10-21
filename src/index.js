e3aa2853cf8b0f2494ed78f7b90cd9a637fc8e6a
Mehmed Kadi <123172570+MehmedKadi@users.noreply.github.com>
2023-10-21 12:01:23 +0300

    CUB - test server run, configurate package.json and ignore files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log("Hello Medi");
+const express = require("express");
+const app = express();
+const PORT = 5050;
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Home Page! :)");
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}...`));
