43d75beb5030e81ccb6de61e4404509a4ef75cb3
Mehmed Kadi <123172570+MehmedKadi@users.noreply.github.com>
2023-10-21 12:33:51 +0300

    CUB- initialize handlebars, set up default views, rename static folder

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 const express = require("express");
+const handlebars = require("express-handlebars");
+
 const app = express();
 const PORT = 5050;
 
+//* Handlebars configuration
+app.engine("hbs", handlebars.engine({ extname: "hbs" }));
+app.set("view engine", "hbs");
+app.set("views", "src/views");
+
+
+
 app.get("/", (req, res) => {
-  res.send("Welcome to Home Page! :)");
+  // res.send("Welcome to Home Page! :)");
+  res.render("index");
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}...`));
