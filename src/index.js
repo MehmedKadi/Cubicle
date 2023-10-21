9d238b66c7df780111b7f01af4a97ef2f28e5c09
Mehmed Kadi <123172570+MehmedKadi@users.noreply.github.com>
2023-10-21 12:51:54 +0300

    CUB- set up express with static files, trim handlebars views

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const handlebars = require("express-handlebars");
+const path = require("path");
 
 const app = express();
 const PORT = 5050;
@@ -9,7 +10,10 @@ app.engine("hbs", handlebars.engine({ extname: "hbs" }));
 app.set("view engine", "hbs");
 app.set("views", "src/views");
 
+//* Set Up static files
+const staticFiles = express.static(path.resolve(__dirname, "public"));
 
+app.use(staticFiles);
 
 app.get("/", (req, res) => {
   // res.send("Welcome to Home Page! :)");
