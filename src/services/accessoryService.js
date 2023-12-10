9b7d64c9bf26a9792511e20ba696d427b86cdf2c
Mehmed Kadi <123172570+MehmedKadi@users.noreply.github.com>
2023-12-10 09:46:13 +0200

    use accessoryService in cubeController, render in dropdown   and clear useless data

--- src/services/accessoryService.js
+++ src/services/accessoryService.js
@@ -1,3 +1,4 @@
 const Accessory = require("../models/Accessory.js");
 
 exports.create = (accessoryData) => Accessory.create(accessoryData);
+exports.getAll = () => Accessory.find();
--- src/services/cubeService.js
+++ src/services/cubeService.js
@@ -1,22 +1,6 @@
 const Cube = require("../models/Cube.js");
 
-const cubes = [
-
-  // {
-  //   id: "1nevib2twlo06r9t6",
-  //   name: "Cube2",
-  //   description: "Cool Shape",
-  //   imageUrl: "https://i.pinimg.com/474x/75/67/75/756775830f533f547e8f7b1267f2111f.jpg",
-  //   difficultyLevel: 3,
-  // },
-  // {
-  //   id: "1nevib1yklo06uanj",
-  //   name: "Red",
-  //   description: "Cool red Cube",
-  //   imageUrl: "https://cubezz.com/images/201801/goods_img/5584_P_1516946006627.jpg",
-  //   difficultyLevel: 2,
-  // },
-];
+const cubes = [];
 
 exports.create = async (cubeData) => {
   // const cube = await Cube(cubeData);
