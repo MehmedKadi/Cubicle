57194a7e70635b31b22ded82881cc4fdb8b26867
Mehmed Kadi <123172570+MehmedKadi@users.noreply.github.com>
2023-12-11 14:22:23 +0200

    hash password before saving in bd

--- src/models/User.js
+++ src/models/User.js
@@ -1,8 +1,21 @@
+const bcrypt = require("bcrypt");
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   username: String,
   password: String,
+});
+
+// TODO If the user already exist, throw Error
+userSchema.virtual("repeatPassword").set(function (value) {
+  if (value !== this.password) {
+    throw new mongoose.MongooseError("Password mismatch!");
+  }
+});
+
+userSchema.pre("save", async function () {
+  const hash = await bcrypt.hash(this.password, 10);
+  this.password = hash;
 });
 
 const User = mongoose.model("User", userSchema);
